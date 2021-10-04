ae807458f4ac2409ac11e2c6a33c905237c32574
Mohit <53290722+mohit-sharma-msg@users.noreply.github.com>
2021-10-04 13:58:57 +0530

    commit

---
+++ Nsproject/nsapp/views.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import InstructorSerializer, CourseSerializer
+from .models import Instructor, Course
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+
+#users = User.objects.all()
+#for user in users:
+#    token = Token.objects.get_or_create(user=user)
+#    print(token)
+
+class WriteByAdminOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        print('inside has permission', request.user)
+        user = request.user
+        if request.method == 'GET':
+            return True
+
+        if request.method == 'POST' or  request.method == 'PUT' or request.method == 'DELETE':
+            if user.is_superuser:
+                return True
+
+        return False
+
+
+class InstructorListView(generics.ListCreateAPIView):
+    serializer_class = InstructorSerializer
+    queryset = Instructor.objects.all()
+
+
+class InstructorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = InstructorSerializer
+    queryset = Instructor.objects.all()
+
+
+class CourseListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated, WriteByAdminOnlyPermission]
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
